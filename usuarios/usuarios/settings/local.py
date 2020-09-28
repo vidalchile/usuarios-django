4c265ac5605d9ed235309be05f11e4c6848e8321
Cristian Vidal <cris.vidal04@gmail.com>
2020-09-28 20:18:05 -0300

    ocultando archivos importantes

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,30 @@
+# importo lo que tiene el archivo base.py
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('DB_USER'),
+        'PASSWORD': get_secret('DB_PASSWORD'),
+        'HOST': get_secret('DB_HOST'),
+        'PORT': get_secret('DB_PORT'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
