95c3d717ec941a62415142da36d40440fb903f5c
Cristian Vidal <cris.vidal04@gmail.com>
2020-10-12 13:32:04 -0300

    clase -> 165. Crear Usuario - FromView -> ok

--- usuarios/applications/users/forms.py
+++ usuarios/applications/users/forms.py
@@ -3,9 +3,31 @@ from django import forms
 from .models import User
 
 class UserRegisterForm(forms.ModelForm):
+     
+    password1 = forms.CharField( 
+        label='Contraseña',
+        required=True, 
+        max_length=32,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Ingresar una contraseña'})
+    )
+
+    password2 = forms.CharField(
+        label='Repetir Contraseña',
+        required=True,
+        max_length=32, 
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repetir contraseña'})
+    )
 
     class Meta:
         model = User
         # muestra todos los campos del modelo
-        fields = ('__all__')        
+        fields = (
+            'username',
+            'email',
+            'nombres',
+            'apellidos',
+            'genero',
+            'password1',
+            'password2',
+        )        
         
